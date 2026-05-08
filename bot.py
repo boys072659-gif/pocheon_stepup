381ce26036b3fcbe1f113105af69f697a33153f5
boys072659-gif <boys072659@gmail.com>
2026-05-09 07:49:22 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -352,6 +352,7 @@ def main():
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
+        url_path="/webhook",
         webhook_url=webhook_url,
         allowed_updates=Update.ALL_TYPES,
     )
